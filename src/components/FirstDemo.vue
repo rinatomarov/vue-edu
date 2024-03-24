620175abdeaae08b637127a1b5d51a864d8a3af5
rinatomarov <rok050600@mail.ru>
2024-03-24 15:34:50 +0500

    create vue project

---
+++ src/components/FirstDemo.vue
@@ -0,0 +1,28 @@
+<template>
+  <button>{{ text }}</button>
+</template>
+
+<script setup>
+// props
+defineProps({
+  text: String
+})
+</script>
+
+<style>
+button {
+  padding: 12px;
+  background: #4646dc;
+  color: #f2f2f2;
+  font-size: medium;
+  font-weight: bold;
+  border-radius: 8px;
+  border: none;
+  margin-top: 12px;
+  width: 150px;
+  //height: 52px;
+  margin-left: 25px;
+  margin: auto;
+  /*justify-content: flex-start;*/
+}
+</style>
---
+++ src/components/SliderImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div><img :src=imagesArray[counter] alt="image" width="400" height="350"></div>
+  <FirstDemo text="<" @click="changeImage(-1)"></FirstDemo>
+  <FirstDemo text=">" @click="changeImage(1)"></FirstDemo>
+</template>
+<script setup>
+import FirstDemo from "@/components/FirstDemo.vue";
+import {ref} from "vue";
+const imagesArray = [
+  'https://media.istockphoto.com/id/183412466/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B2%D0%BE%D1%81%D1%82%D0%BE%D1%87%D0%BD%D1%8B%D0%B5-bluebirds-%D0%BC%D1%83%D0%B6%D1%81%D0%BA%D0%BE%D0%B3%D0%BE-%D0%B8-%D0%B6%D0%B5%D0%BD%D1%81%D0%BA%D0%BE%D0%B3%D0%BE-%D0%BF%D0%BE%D0%BB%D0%B0.jpg?s=612x612&w=0&k=20&c=h67A1VxL4ca0AztbhFCJlEcLSr3QMmTmMn0Cmu6PS1M=',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1TAN8OtW4W7nW2aaA57-DoHiJIel4UcI9OZQWstxFyw&s',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3RtYYcHG0c74yS80R6c5tn-KdeRseSdjpGvY62GWgcQ&s',
+  'https://content.skyscnr.com/m/30c17f9542f75237/original/GettyImages-148199249.jpg?resize=2560px:1693px'
+]
+let counter = ref(0)
+const changeImage = (nextOrPrevDirection) => {
+  counter.value = (counter.value + nextOrPrevDirection + imagesArray.length) % imagesArray.length
+}
+</script>
+<style scoped> </style>
