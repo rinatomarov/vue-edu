62152cdbf50220800b76868bde43020ea8074cd7
rinatomarov <rok050600@mail.ru>
2024-04-02 02:01:42 +0500

    changes

---
+++ src/App_1.vue
@@ -0,0 +1,55 @@
+<template>
+<!--    <Counter></Counter>-->
+<!--    <Quotations></Quotations>-->
+<!--    <Counter></Counter>-->
+<!--    <p>Hello World</p>-->
+
+<!--    <FirstDemo text="Нажми меня"></FirstDemo>-->
+<!--    <FirstDemo text="Press me"></FirstDemo>-->
+<!--    <FirstDemo text="Submit"></FirstDemo>-->
+
+<!--    <AddImage></AddImage>-->
+
+<!--  <SliderImage></SliderImage>-->
+
+<!--    <AddCards></AddCards>-->
+<!--  <InputMailForm></InputMailForm>-->
+<!--  <PrintMail></PrintMail>-->
+  <!--<PasswordValidation></PasswordValidation>-->
+    <ModalSignPage v-model:login="login" v-model:passWord="passWord" >
+      <template #header>
+          <p>Прошу ввести ваши регистрационные данные:</p>
+      </template>
+      <template>
+      </template>
+      <template #footer>
+        <button>Перейти в сайт</button>
+      </template>
+    </ModalSignPage>
+  <h1>Login : {{login}}</h1>
+  <h1>PassWord : {{passWord}}</h1>
+</template>
+
+<script setup>
+// import FirstDemo from "@/components/FirstDemo.vue";
+// import Counter from "@/components/Counter.vue";
+// import Quotations from "@/components/icons/Quotations.vue";
+import AddImage from "@/components/AddImage.vue";
+import SliderImage from "@/components/SliderImage.vue";
+import AddCards from "@/components/AddCards.vue";
+// import PrintEmail from "@/components/InputMailForm.vue";
+// import InputMailForm from "@/components/InputMailForm.vue";
+import PrintMail from "@/components/PrintMail.vue";
+import PasswordValidation from "@/components/PasswordValidation.vue";
+import { computed, ref } from "vue";
+import ModalSignPage from "@/components/ModalSignPage.vue";
+const login = ref('')
+const passWord = ref('')
+
+</script>
+
+<style>
+p {
+  color: red;
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,17 @@
+<template>
+  <table border="1">
+    <tr v-for="row in items">
+      <td v-for="(col, field) in row">
+        {{ col }}
+        <slot :cell="col"></slot>
+        <slot :name="field" :cell="col" :row="row"></slot>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script setup>
+defineProps({
+  items: Array
+})
+</script>
