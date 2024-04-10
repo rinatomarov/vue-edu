0043bc45ef3931f65fb208bf0a4e0a3db854fc5b
rinatomarov <rok050600@mail.ru>
2024-04-10 19:10:01 +0500

    btn

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <ButtonSizeBackground ref="btnBg"></ButtonSizeBackground>
+</template>
+
+<script setup>
+import ButtonSizeBackground from "@/components/ButtonSizeBackground.vue";
+import {onMounted, ref} from "vue";
+const btnBg =  ref()
+console.log(btnBg.value)
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ButtonSizeBackground.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <button @click="color='buttonBtn'">Button</button>
+
+  <h1>Размер</h1>
+  <button @click="size='size'">S</button>
+  <button @click="size='medium'">M</button>
+  <button @click="size='purple'">L</button>
+</template>
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
